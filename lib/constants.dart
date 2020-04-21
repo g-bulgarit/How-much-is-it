9cfd6d1cdc5fefe4dd659d2db0f5f06bb03803b7
g-bulgarit <alonio6@gmail.com>
2020-04-21 19:27:48 +0300

    added some constants to begin converting stuff

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+
+const conversion_set = {
+  // Energy - convert to watts
+  "watts" : 1,
+  "horsepower" : 5,
+  "mega watts" : 1000,
+  "giga watts" : 1000000,
+
+  // Physical size - convert to meters
+  "milimeters" : 0.001,
+  "centimeters" : 0.01,
+  "kilometers" : 1,
+  "miles" : 1.6,
+
+  // Fluids - convert to liters
+  "mililiters" : 0.001,
+};
