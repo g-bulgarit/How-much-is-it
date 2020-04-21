ac232fbac45f259889e4998b43383c1550edc0b8
g-bulgarit <alonio6@gmail.com>
2020-04-21 19:27:29 +0300

    Basic user input recognition now works

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,15 @@ void main(){
 
 class TextInput extends StatefulWidget {
   @override
-  MyTextState createState() => new MyTextState();
+  UserTextInput createState() => new UserTextInput();
 
 }
 
-class MyTextState extends State<TextInput>{
 
-  String lookup = "";
+class UserTextInput extends State<TextInput>{
+  String userText = "";
+  double userNumber = 0.0;
+
   final TextEditingController controller = new TextEditingController();
 
   @override
@@ -37,16 +39,50 @@ class MyTextState extends State<TextInput>{
                 ),
                 onSubmitted: (String str){
                   setState(() {
-                    lookup = str;
+                    //final Tuple2<double, String> result = this.praseText(str);
+                    List result = this.praseText(str);
+                    userNumber = result[0];
+                    userText = result[1];
                   });
+                  // Remove text from textbox
                   controller.text = "";
                 },
               ),
-              new Text(lookup)
+              new Text("(" + this.userText + "," + this.userNumber.toString() + ")")
             ] 
           ),
         ),
       )
     );
+  }
+
+  // Tuple2 <double, String> praseText(String inputStr){
+  //   // Function to parse user input into <quantity> and <attribute>
+
+  //   double quantity = 0.0;
+  //   String attribute = "";
+
+  //   RegExp extractQuantity = RegExp(r'^[0-9]+');
+  //   RegExp extractAttribute = RegExp(r'[A-Za-z]+');
+
+  //   quantity = double.parse(extractQuantity.firstMatch(inputStr).group(0));
+  //   attribute = extractAttribute.firstMatch(inputStr).group(0);
+
+  //   return new Tuple2(quantity, attribute);
+  // }
+
+  List praseText(String inputStr){
+    // Function to parse user input into <quantity> and <attribute>
+
+    double quantity = 0.0;
+    String attribute = "";
+
+    RegExp extractQuantity = RegExp(r'^[0-9]+');
+    RegExp extractAttribute = RegExp(r'[A-Za-z]+');
+
+    quantity = double.parse(extractQuantity.firstMatch(inputStr).group(0));
+    attribute = extractAttribute.firstMatch(inputStr).group(0);
+
+    return [quantity, attribute];
   }
 }
