73622be38f43ff1c2aa6d4d03aa8a9036ef09dfd
g-bulgarit <alonio6@gmail.com>
2020-04-21 23:40:32 +0300

    added more constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,20 +1,73 @@
 
-final convertFrom = {"energy"           : {"watts" : 1,
+final convertFrom = {"power"           : { "watts" : 1,
                                            "horsepower" : 5,
                                            "megawatts" : 1000,
-                                           "gigawatts" : 1000000},
+                                           "gigawatts" : 1000000,
+                                           },
+
+                     "energy"          : {"joule" : 1,
+                                          "electron volt" : 1,
+                                          "kcal" : 4200,
+                     },
       
-                      "physical"        : {"milimeters" : 0.001,
-                                           "centimeters" : 0.01,
-                                           "kilometers" : 1,
-                                           "miles" : 1.6},
-      
-                      "fluids"          : {"mililiters" : 0.001,}
+                     "length"        : {"meter":1,
+                                          "milimeters" : 0.001,
+                                          "centimeters" : 0.01,
+                                          "kilometers" : 1,
+                                          "miles" : 1.6,
+                                          "inches" : 39.37,
+                                          "foot": 3.28084,
+                                          "feet" : 5},
+     
+                     "volume"          : {"liter":1,
+                                          "litre": 1,
+                                          "mililiters" : 0.001,
+                                          "oz" : 33.814,
+                                          "pint": 0.473,
+                                          "gallon": 3.78541,},
+                                          
+                     "mass"            : {"kilograms": 1,
+                                          "gram": 0.001,},
+
+                     "velocity"        : {"meters per second" : 1,
+                                          "": 4,},
+                     "force"           : {"newtons":1},
+                     "pressure"        : {"a":2},
+                     "electricity"     : {"a":2},
                    };
 
 
-final convertTo = {"energy"          :{"hamsters on a wheel" : 0.015,
-                                        "AK47 recoil" : 124,
-                                        "mashing keys on a keyboard" : 40},
-                    "physical"       :{},
-                    "fluids"         :{}};
+final convertTo = {"power"          :{"hamsters on a wheel" : 0.015,
+                                       "mashing keys on a keyboard" : 40,
+                                       "AA batteries": 2},
+
+                    "energy"        : {"recoil from an AK47 shot" : 2108,
+                                       "":4},
+
+                    "length"       :{"pinky fingers": 0,
+                                       "": 2},
+                    "volume"         :{"total lung capacity of an adult": 6,
+                                       "first computer ever built":5 ,},
+
+                    "mass"            :{"times the mass of the universe":2,
+                                        "times the mass of the average whale":5,
+                                        "times the mass of a single toilet paper square":5,
+                                        "in single dollar bills":3,
+                                        "in (WW2) tiger-II tanks":68500},
+
+                    "velocity"       :{"times the average speed of a turtle":2,
+                                       "times the average speed of a rabbit":3,
+                                       "times the record speed of Usain Bolt":3,
+                                       "times the fastest record pitch in baseball":3,
+                                       "times the speed in which toenails grow":3,
+                                       "times the average speed of a sloth":5,
+                                       "times the speed of a snake's tounge":5},
+
+                    "force"          :{"times the maximum g-force on a fighter jet ":2,
+                                       "times the average professional boxer's punch":5,
+                                       "times the force of an averge soccer kick": 5},
+
+                    "pressure"       :{"a":2},
+
+                    "electricity"    :{"a":2},
+                    };
