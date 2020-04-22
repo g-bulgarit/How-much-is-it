3dd0279c4eb64a2da68df5e5d2c3c0e992443c12
g-bulgarit <alonio6@gmail.com>
2020-04-22 21:08:26 +0300

    fixed some constants and colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -9,6 +9,6 @@ const Color foregroundColorMain = Color(0xFF8EE4AF);
 const Color foregroundColorDark = Color(0xFF05386B);
 // Text colors
 const Color textColorMain = Color(0xFFEDF5E1);
-const Color textColorHint = Color(0xFFf0f7e7);
+const Color textColorHint = Color(0xFF379683);
 // Ref
 // https://visme.co/blog/wp-content/uploads/2016/09/website25-1024x512.jpg
--- lib/constants.dart
+++ lib/constants.dart
@@ -10,24 +10,24 @@ final convertFrom = {"power"           : { "watts" : 1,
                                           "kcal" : 4200,
                                          },
       
-                     "length"          : {"meter":1,
+                     "length"          : {"meters":1,
                                           "milimeters" : 0.001,
                                           "centimeters" : 0.01,
                                           "kilometers" : 1000,
                                           "miles" : 1.6,
                                           "inches" : 39.37,
                                           "foot": 3.28084,
-                                          "feet" : 5},
+                                          "feet" : 3.28084},
      
-                     "volume"          : {"liter":1,
-                                          "litre": 1,
+                     "volume"          : {"liters":1,
+                                          "litres": 1,
                                           "mililiters" : 0.001,
                                           "oz" : 33.814,
                                           "pints": 0.473,
                                           "gallons": 3.78541,},
                                           
                      "mass"             : {"kilograms": 1,
-                                           "gram": 0.001,
+                                           "grams": 0.001,
                                           },
 
                      "velocity"        : {"meters per second" : 1,
@@ -39,31 +39,40 @@ final convertFrom = {"power"           : { "watts" : 1,
                      "pressure"        : {"a":2},
 
                      "electricity"     : {"a":2},
+                     "time"            : {"seconds" : 1,
+                                          "minutes": 60,
+                                          "hours" : 3600,
+                                          "days" : 86400,
+                                          "years": 31536000 }
                    };
 
 
-final convertTo = {"power"             : {"hamsters on a wheel" : 0.015,
-                                          "keystrokes on a keyboard" : 0.0019,
-                                          "AA batteries": 2
+final convertTo = {"power"             : {"hamsters on a wheel" : 0.4,
+                                          "keystrokes on a keyboard (per second)" : 0.0015,
+                                          "AA batteries (per hour)": 3.9
                                          },
 
-                    "energy"           : {"recoil from an AK47 shot" : 2108,
+                    "energy"           : {"times the recoil from an AK47 shot" : 2108,
                                           "":4
                                          },
 
                     "length"           :{"widths of a pinky finger": 0.0127,
-                                         "Boeing 747 jets, stacked cockpit to tail": 71
+                                         "Boeing 747 jets, stacked cockpit to tail": 71,
+                                         "times the length of a giraffe's tounge": 0.4826,
+                                         "times the length of Chile": 4270000,
+                                         "times the width of the Statue of Liberty's mouth": 0.9144,
+                                         "times the length of Yonge street, Toronto, Canada" : 1896000,
                                         },
 
                     "volume"           :{"times the total lung capacity of an adult male": 6,
                                          "first computer ever built":5 ,
                                         },
 
-                    "mass"             :{"times the mass of the universe":2,
-                                         "times the mass of the average whale":5,
-                                         "times the mass of a single toilet paper square":5,
-                                         "single dollar bills":3,
-                                         "(WW2) tiger-II tanks":68500
+                    "mass"             :{"times the mass of the universe":1.5*10e56,
+                                         "times the mass of the average adult blue whale":150000000,
+                                         "times the mass of a single-ply toilet paper square":0.0005,
+                                         "(WW2) tiger-II tanks":68500,
+                                         "single dollar bills": 0.001
                                         },
 
                     "velocity"         :{"times the average speed of a turtle":2,
@@ -85,4 +94,7 @@ final convertTo = {"power"             : {"hamsters on a wheel" : 0.015,
 
                     "electricity"      :{"a":2,
                                         },
+
+                    "time"             :{"times the longest recorded flight of a chicken" : 13,
+                                         "":1}
                   };
