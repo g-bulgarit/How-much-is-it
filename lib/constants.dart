724de279a4ca67d87625cd1ccb422548eed23e13
g-bulgarit <alonio6@gmail.com>
2020-04-22 19:31:40 +0300

    fixed some constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,17 +3,17 @@ final convertFrom = {"power"           : { "watts" : 1,
                                            "horsepower" : 5,
                                            "megawatts" : 1000,
                                            "gigawatts" : 1000000,
-                                           },
+                                         },
 
                      "energy"          : {"joule" : 1,
                                           "electron volt" : 1,
                                           "kcal" : 4200,
-                     },
+                                         },
       
-                     "length"        : {"meter":1,
+                     "length"          : {"meter":1,
                                           "milimeters" : 0.001,
                                           "centimeters" : 0.01,
-                                          "kilometers" : 1,
+                                          "kilometers" : 1000,
                                           "miles" : 1.6,
                                           "inches" : 39.37,
                                           "foot": 3.28084,
@@ -23,51 +23,66 @@ final convertFrom = {"power"           : { "watts" : 1,
                                           "litre": 1,
                                           "mililiters" : 0.001,
                                           "oz" : 33.814,
-                                          "pint": 0.473,
-                                          "gallon": 3.78541,},
+                                          "pints": 0.473,
+                                          "gallons": 3.78541,},
                                           
-                     "mass"            : {"kilograms": 1,
-                                          "gram": 0.001,},
+                     "mass"             : {"kilograms": 1,
+                                           "gram": 0.001,
+                                          },
 
                      "velocity"        : {"meters per second" : 1,
-                                          "": 4,},
+                                          "": 4,
+                                         },
+
                      "force"           : {"newtons":1},
+
                      "pressure"        : {"a":2},
+
                      "electricity"     : {"a":2},
                    };
 
 
-final convertTo = {"power"          :{"hamsters on a wheel" : 0.015,
-                                       "mashing keys on a keyboard" : 40,
-                                       "AA batteries": 2},
+final convertTo = {"power"             : {"hamsters on a wheel" : 0.015,
+                                          "keystrokes on a keyboard" : 0.0019,
+                                          "AA batteries": 2
+                                         },
 
-                    "energy"        : {"recoil from an AK47 shot" : 2108,
-                                       "":4},
+                    "energy"           : {"recoil from an AK47 shot" : 2108,
+                                          "":4
+                                         },
 
-                    "length"       :{"pinky fingers": 0,
-                                       "": 2},
-                    "volume"         :{"total lung capacity of an adult": 6,
-                                       "first computer ever built":5 ,},
+                    "length"           :{"widths of a pinky finger": 0.0127,
+                                         "Boeing 747 jets, stacked cockpit to tail": 71
+                                        },
 
-                    "mass"            :{"times the mass of the universe":2,
-                                        "times the mass of the average whale":5,
-                                        "times the mass of a single toilet paper square":5,
-                                        "in single dollar bills":3,
-                                        "in (WW2) tiger-II tanks":68500},
+                    "volume"           :{"times the total lung capacity of an adult male": 6,
+                                         "first computer ever built":5 ,
+                                        },
 
-                    "velocity"       :{"times the average speed of a turtle":2,
-                                       "times the average speed of a rabbit":3,
-                                       "times the record speed of Usain Bolt":3,
-                                       "times the fastest record pitch in baseball":3,
-                                       "times the speed in which toenails grow":3,
-                                       "times the average speed of a sloth":5,
-                                       "times the speed of a snake's tounge":5},
+                    "mass"             :{"times the mass of the universe":2,
+                                         "times the mass of the average whale":5,
+                                         "times the mass of a single toilet paper square":5,
+                                         "single dollar bills":3,
+                                         "(WW2) tiger-II tanks":68500
+                                        },
 
-                    "force"          :{"times the maximum g-force on a fighter jet ":2,
-                                       "times the average professional boxer's punch":5,
-                                       "times the force of an averge soccer kick": 5},
+                    "velocity"         :{"times the average speed of a turtle":2,
+                                         "times the average speed of a rabbit":3,
+                                         "times the record speed of Usain Bolt":3,
+                                         "times the fastest record pitch in baseball":3,
+                                         "times the speed in which toenails grow":3,
+                                         "times the average speed of a sloth":5,
+                                         "times the speed of a snake's tounge":5
+                                        },
 
-                    "pressure"       :{"a":2},
+                    "force"            :{"times the maximum g-force on a fighter jet ":2,
+                                         "times the average professional boxer's punch":5,
+                                         "times the force of an averge soccer kick": 5
+                                        },
 
-                    "electricity"    :{"a":2},
-                    };
+                    "pressure"         :{"a":2,
+                                        },
+
+                    "electricity"      :{"a":2,
+                                        },
+                  };
